1ded04e25dc4f8d5038ef28eae7cf51a0141d5aa
ruucm <ruucm.a@gmail.com>
2017-09-27 19:36:16 +0900

    Init Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { MomentModule } from 'angular2-moment';
+import { ChatsPage } from '../pages/chats/chats';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ChatsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    MomentModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ChatsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/chats/chats.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Chats, Messages } from 'api/collections';
+import { Chat } from 'api/models';
+import { Observable } from 'rxjs';
+
+@Component({
+  templateUrl: 'chats.html'
+})
+export class ChatsPage implements OnInit {
+  chats; 
+  constructor() {}
+  removeChat(chat: Chat): void {
+	  Chats.remove({_id: chat._id}).subscribe(() => {
+    });
+  }
+  ngOnInit() {
+    this.chats = Chats
+      .find({})
+      .mergeMap((chats: Chat[]) =>
+        Observable.combineLatest(
+          ...chats.map((chat: Chat) =>
+            Messages
+              .find({chatId: chat._id})
+              .startWith(null)
+              .map(messages => {
+                if (messages) chat.lastMessage = messages[0];
+                return chat;
+              })
+          )
+        )
+      ).zone();
+  }
+}
